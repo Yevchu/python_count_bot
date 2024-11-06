9325d7b3d8fefe649a3377c6b7fb05ac6d4c30c1
Vitalyi <evciu97@gmail.com>
2024-11-06 16:13:40 +0200

    виправлено граматичні помилки

--- db_config.py
+++ db_config.py
@@ -10,9 +10,9 @@ from sqlalchemy.exc import IntegrityError
 
 load_dotenv()
 
-DATABASE_URT = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv("DATABASE_URL")  # Переконайтесь, що змінна називається DATABASE_URL
 
-engine = create_engine(DATABASE_URT)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
